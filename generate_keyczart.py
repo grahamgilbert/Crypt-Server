cac3099b9ab07d5ac2180e0b2796f55668ddda1e
Graham Gilbert <graham@grahamgilbert.com>
2015-07-14 08:52:28 +0100

    Make the directory if it doesn't exist

--- generate_keyczart.py
+++ generate_keyczart.py
@@ -2,8 +2,11 @@ import keyczar
 from keyczar import keyczart
 import os
 directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'keyset')
-if not os.listdir(directory):
+
+if not os.path.exists(directory):
     os.makedirs(directory)
+
+if not os.listdir(directory):
     keyczart.main(['create','--location=keyset','--purpose=crypt','--name=crypt'])
     keyczart.main(['addkey','--location=keyset' ,'--status=primary'])
 else:
