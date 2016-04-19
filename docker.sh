0164791ebfddfabdc2eb10f16788ca3149a50abb
Graham Gilbert <graham@airbnb.com>
2016-04-19 11:26:12 +0100

    Quick clean up of docker start script

--- docker.sh
+++ docker.sh
@@ -7,9 +7,9 @@ CWD=`pwd`
 
 
 # Check that Docker Machine exists
-
+# --vmwarefusion-boot2docker-url "https://github.com/boot2docker/boot2docker/releases/download/v1.8.2/boot2docker.iso"
 if [ -z "$(docker-machine ls | grep crypt)" ]; then
-  docker-machine create -d vmwarefusion --vmwarefusion-disk-size=500000 --vmwarefusion-boot2docker-url "https://github.com/boot2docker/boot2docker/releases/download/v1.8.2/boot2docker.iso" --vmwarefusion-memory-size=2048 --vmwarefusion-cpu-count=4 crypt
+  docker-machine create -d vmwarefusion --vmwarefusion-disk-size=500000 --vmwarefusion-memory-size=2048 --vmwarefusion-cpu-count=4 crypt
   docker-machine env crypt
   eval "$(docker-machine env crypt)"
 fi
@@ -27,7 +27,7 @@ IP=`docker-machine ip crypt`
 
 docker ps -aq | xargs docker rm -f
 KEYSET=$CWD/keyset
-docker build -t "macadmins/imagr-server" $CWD
+docker build -t "macadmins/crypt-server" $CWD
 docker run -d \
   --name="crypt" \
   -e ADMIN_PASS='password' \
