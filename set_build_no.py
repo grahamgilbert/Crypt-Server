12b94273d5cf5109fc6e27703a9a40184d819508
Graham Gilbert <graham@grahamgilbert.com>
2018-12-03 08:45:43 -0800

    Version in footer (#62)
    * Set build number
    
    * Actual build number
    
    * Add version to template

---
+++ set_build_no.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import plistlib
+import subprocess
+
+current_version = "2.4.2"
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+
+# based on http://tgoode.com/2014/06/05/sensible-way-increment-bundle-version-cfbundleversion-xcode
+
+print("Setting Version to Git rev-list --count")
+cmd = ["git", "rev-list", "HEAD", "--count"]
+build_number = subprocess.check_output(cmd)
+# This will always be one commit behind, so this makes it current
+build_number = int(build_number) + 1
+
+version_number = "{}.{}".format(current_version, build_number)
+
+data = {"version": version_number}
+plist_path = "{}/fvserver/version.plist".format(script_path)
+plistlib.writePlist(data, plist_path)
