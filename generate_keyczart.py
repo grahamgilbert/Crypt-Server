b2e8c2c18f5935c548d750794fdfd83e74c72111
Graham Gilbert <graham@grahamgilbert.com>
2018-12-17 18:19:05 +0000

    fix keyczart

--- generate_keyczart.py
+++ generate_keyczart.py
@@ -1,5 +1,5 @@
 import keyczar
-from keyczar import tool as keyczart
+import subprocess
 import os
 directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'keyset')
 
@@ -7,7 +7,16 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 
 if not os.listdir(directory):
-    keyczart.main(['create','--location=keyset','--purpose=crypt','--name=crypt'])
-    keyczart.main(['addkey','--location=keyset' ,'--status=primary'])
+    location_string = '--location={}'.format(directory)
+    cmd = [
+        'keyczart',
+        'create',
+        location_string,
+        '--purpose=crypt',
+        '--name=crypt'
+    ]
+    subprocess.check_call(cmd)
+    cmd = ['keyczart', 'addkey',location_string ,'--status=primary']
+    subprocess.check_call(cmd)
 else:
     print('Keyset directory already has something in there. Skipping key generation.')
