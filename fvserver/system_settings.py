836092dadbef4accdbd3efbd6071ce8f568f7665
Graham Gilbert <graham@grahamgilbert.com>
2018-12-18 21:52:45 +0000

    Make linter happy

--- fvserver/system_settings.py
+++ fvserver/system_settings.py
@@ -120,7 +120,7 @@ TEMPLATES = [
     }
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
